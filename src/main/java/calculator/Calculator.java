8b3fbfc87bea8a357ba1728d51c8293d031f33e2
Mateusz Parkitny <parkitny.mateusz@gmail.com>
2018-01-23 20:15:13 +0100

    renamed methods

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,19 +1,19 @@
 package calculator;
 
 public class Calculator {
-    public float plus(float a, float b) {
+    public float add(float a, float b) {
         return a + b;
     }
 
-    public float minus(float a, float b) {
+    public float subtract(float a, float b) {
         return a - b;
     }
 
-    public float times(float a, float b) {
+    public float multiply(float a, float b) {
         return a * b;
     }
 
-    public float by(float a, float b) {
+    public float divide(float a, float b) {
         if (b == 0f) {
             throw new IllegalArgumentException("Division by 0!");
         }
@@ -25,8 +25,16 @@ public class Calculator {
         for (int i = 1; i < b; i++) {
             result *= a;
         }
+        return result;
+    }
 
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
 
+    public double cubeRoot(double a) {
+        return Math.cbrt(a);
+    }
 }
 
 
