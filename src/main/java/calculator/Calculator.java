fec64f0dc746705a0d07e08f995cda778462ab85
Mateusz Parkitny <parkitny.mateusz@gmail.com>
2018-01-23 18:57:27 +0100

    raise method modified

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -18,7 +18,8 @@ public class Calculator {
     }
     public int raise(int a, int b) { int result = a; for (int i = 1; i < b; i++) {  result *= a; }
 
-        return result; }
+
+    return result; }
 }
 
 
