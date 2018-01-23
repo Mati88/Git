0beaa275399575878afc41936ba4ee396d7c843e
Mateusz Parkitny <parkitny.mateusz@gmail.com>
2018-01-23 18:53:50 +0100

    initial commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+
+    public float minus(float a, float b) {
+        return a - b;
+    }
+
+    public float times(float a, float b) {
+        return a * b;
+    }
+
+    public float by(float a, float b) {
+        return a / b;
+    }
+    public int raise(int a, int b) { int result = a; for (int i = 1; i < b; i++) {  result *= a; }
+
+        return result; }
+}
+
+
