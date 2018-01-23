3435c5876f17cafdf570795afd1dbae958aabb99
Mateusz Parkitny <parkitny.mateusz@gmail.com>
2018-01-23 19:04:15 +0100

    fixed dividing by zero in Calculator/by method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -14,12 +14,21 @@ public class Calculator {
     }
 
     public float by(float a, float b) {
+        if (b == 0f) {
+            throw new IllegalArgumentException("Division by 0!");
+        }
         return a / b;
     }
-    public int raise(int a, int b) { int result = a; for (int i = 1; i < b; i++) {  result *= a; }
+
+    public int raise(int a, int b) {
+        int result = a;
+        for (int i = 1; i < b; i++) {
+            result *= a;
+        }
 
 
-    return result; }
+        return result;
+    }
 }
 
 
